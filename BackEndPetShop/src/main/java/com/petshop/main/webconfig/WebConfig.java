9bb39c4ee2785f8d80225033b09ba4457a24c938
Maurício Dessuy <mauriciodessuy@hotmail.com>
2019-03-17 22:33:34 -0300

    Adição de classe para tratar headers de mensagens REST
       * Foi adicionado uma classe para configurar os headers das mensagens REST.
       * Pois estava ocorrendo um erro ao realizar chamadas do FrontEnd (localhost:4200) para o BackEnd (localhost:8080).
       * Então esta nova classe faz a adição do parâmetro "Allow-Control-Allow-Origin" com o valor "*" para evitar o bloqueio do CORS.

---
+++ BackEndPetShop/src/main/java/com/petshop/main/webconfig/WebConfig.java
@@ -0,0 +1,21 @@
+package com.petshop.main.webconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author MuMau
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
