16678aa1745d6e0f2c74dc819a5b83c8d00005bc
Maurício Dessuy <mauriciodessuy@hotmail.com>
2019-03-24 15:49:18 -0300

    Adição de comentários de documentação
    * Na classe WebConfig foi adicionado os comentários de documentação para explicar o motivo e o funcionamento da classe.

--- BackEndPetShop/src/main/java/com/petshop/main/webconfig/WebConfig.java
+++ BackEndPetShop/src/main/java/com/petshop/main/webconfig/WebConfig.java
@@ -6,7 +6,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- *
+ * Classe utilizada para adicionar configurações nas mensagens REST.<br>
+ * Pois a validação CORS não estava permitindo mandar mensagens em um mesmo servidor porém utilizando portas diferentes.<br>
+ * Por exemplo: localhost:4200 para localhost:8080.
  * @author MuMau
  */
 @Configuration
@@ -14,6 +16,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
+    /**
+     * Método que adiciona as permissões no header da mensagens REST.
+     */
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
