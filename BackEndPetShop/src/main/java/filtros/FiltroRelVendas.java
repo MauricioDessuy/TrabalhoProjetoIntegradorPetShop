3be77bddbe1f138a99e9adc3c7dbea983199dc43
jose <jose.mattioni@gmail.com>
2019-05-29 20:04:14 -0300

    Relatório vendas
    adiciona filtros para o relatório de vendas

---
+++ BackEndPetShop/src/main/java/filtros/FiltroRelVendas.java
@@ -0,0 +1,35 @@
+package filtros;
+
+import java.util.Date;
+
+public class FiltroRelVendas {
+
+    private Date periodoInicial;
+    private Date periodoFinal;
+    private int formaPagamento;
+
+    public int getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(int formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Date getPeriodoInicial() {
+        return periodoInicial;
+    }
+
+    public void setPeriodoInicial(Date periodoInicial) {
+        this.periodoInicial = periodoInicial;
+    }
+
+    public Date getPeriodoFinal() {
+        return periodoFinal;
+    }
+
+    public void setPeriodoFinal(Date periodoFinal) {
+        this.periodoFinal = periodoFinal;
+    }
+
+}
