96c595b505b0e1090464e844d25317bee783ce4e
Mauricio Dessuy <mauriciodessuy@hotmail.com>
2019-06-19 19:37:40 -0300

    Adicionado script para criação de tabelas
    * Foi adicionado um script contendo a criação das tabelas do sistema.

---
+++ BackEndPetShop/src/main/resources/BancoDados.sql
@@ -0,0 +1,40 @@
+CREATE TABLE pessoas
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    cpf VARCHAR(255),
+    data_nascimento DATE,
+    email VARCHAR(255),
+    nome VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    sexo INTEGER NOT NULL,
+    telefone VARCHAR(255),
+    usuario VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE produtos
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    marca VARCHAR(255) NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    valor_unitario REAL NOT NULL
+)
+
+CREATE TABLE public.fornecedores
+(
+    id SERIAL NOT NULL,
+    cnpj VARCHAR(255)   ,
+    email character varying(255) COLLATE pg_catalog."default",
+    nome character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    telefone character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT fornecedores_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE animals
+(
+    id SERIAL NOT NULL primary key,
+    data_nascimento date,
+    data_obito date,
+    nome character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    raca character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    id_pessoa bigint NOT NULL
+)
